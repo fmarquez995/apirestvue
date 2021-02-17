e69f6d46e1e4d9f8f40bbefde0d9c8d4cef10d0e
fmarquez995 <fmarquez995@gmail.com>
2021-02-17 18:07:15 -0300

    Se crea estilo de formulario

--- frontend-vue/src/components/formularioComponent.vue
+++ frontend-vue/src/components/formularioComponent.vue
@@ -1,13 +1,43 @@
 <template>
-    <section id="content">
-        <h2 class="subheader">Formulario </h2>
-    </section>
+  <section id="content">
+    <h2 class="subheader">Formulario</h2>
+    <form id="formBlog">
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="formGroupExampleInput">Titulo</label>
+          <input
+            type="text"
+            class="form-control"
+            id="titleInput"
+            placeholder="Titulo"
+          />
+        </div>
+      </div>
+
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="formGroupExampleInput2">Contenido</label>
+          <input
+            type="text"
+            class="form-control"
+            id="contentInput"
+            placeholder="Contenido"
+          />
+        </div>
+      </div>
+    </form>
+  </section>
 </template>
 
 <script>
 export default {
-
-name:"formularioComponent"
-
+  name: "formularioComponent",
 };
 </script>
+
+<style scoped>
+#formBlog {
+  padding-left: 26%;
+  background: #eee;
+}
+</style>
